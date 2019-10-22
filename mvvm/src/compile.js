c7692490799bee574aa204bf08f4567873e17dd1
ranckprogram <1474574022@qq.com>
2019-10-22 21:18:30 +0800

    '发布订阅模式简单函数式实现'

--- mvvm/src/compile.js
+++ mvvm/src/compile.js
@@ -1,12 +1,50 @@
 function compile (el) {
-
+  const vNode = node2Fragment(el)
+  compileElements(vNode)
 }
 
-function node2Fragment(el) {
-  let child = null;
-  let frame = document.createDocumentFragment();
-  while(child = el.firstChild) {
+
+/**
+ * 将dom上的元素转移到内存中
+ * @param {Element} el 
+ * @return {Element} frame
+ */
+function node2Fragment (el) {
+  let child = null
+  let frame = document.createDocumentFragment()
+  while (child = el.firstChild) {
     frame.appendChild(child)
   }
+  // console.log(frame.childNodes)
   return frame
+}
+
+function compileElements (vNode) {
+  const reg = /\{\{(.*)\}\}/;
+
+  Array.from(vNode.childNodes).forEach(node => {
+    console.log(node)
+    if(isElement(node)){
+      compile(node)
+    }
+    if (isText(node)) {
+
+    }
+  })
+}
+
+function isElement (node) {
+  return node.nodeType === 1
+}
+
+function isAttr (node) {
+  return node.nodeType === 2
+}
+
+function isText (node) {
+  return node.nodeType === 3
+}
+
+function bind() {
+  
 }
