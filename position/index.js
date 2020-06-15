c50b116976e0fb6d964061e82f1899401c23bbc5
ranckprogram <1474574022@qq.com>
2020-06-15 15:08:01 +0800

    use amap get geolocation

---
+++ position/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
+
+//同步读取密钥和签名证书
+var options = {
+	key: fs.readFileSync('../ca/server.key'),
+	cert: fs.readFileSync('../ca/server.crt')
+};
+
+var app = express();
+var httpsServer = https.createServer(options, app);
+var httpServer = http.createServer(app);
+
+
+app.use(express.static('../position'));
+
+app.get('/', function(req, res, next) {
+	res.send('Hello Express+https');
+});
+//https监听3000端口
+httpsServer.listen(3000);
+//http监听3001端口
+httpServer.listen(3001);
